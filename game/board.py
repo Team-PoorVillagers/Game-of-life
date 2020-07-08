24dc4153349b76cd408c5b679ec050f8bcf97ed4
vishvanath45 <visitdutt@gmail.com>
2020-07-08 23:33:40 +0530

    [vishva|hasan] Feat: Get count of alive neighbours of a cell

--- game/board.py
+++ game/board.py
@@ -1,5 +1,6 @@
 from game.cell import Cell
 from game.status import Status
+from game.point import Point
 
 class Board:
     
@@ -12,5 +13,36 @@ class Board:
             board_rows = [Cell() for i in range(self.columns)]
             self.board.append(board_rows)
 
-        for i in range(len(points)):
-            self.board[points[i].X()][points[i].Y()].set_state(Status.ALIVE)
+        for point in points:
+            self.set_status_of_cell_at_position(point, Status.ALIVE)
+
+    def set_status_of_cell_at_position(self, point, state):
+        return self.board[point.X()][point.Y()].set_state(state)
+
+    def get_status_of_cell_at_position(self, point):
+        return self.board[point.X()][point.Y()].get_state()
+
+    def get_count_of_alive_neighbours_of_cell(self, point):
+        direction_array_x = [1, 1, 0, -1, -1, -1, 0, 1]
+        direction_array_y = [0, -1, -1, -1, 0, 1, 1, 1]
+
+        alive_neighbours = 0
+        for idx in range(0, len(direction_array_x)):
+
+            neighbour_cell_pos = Point(point.X() + direction_array_x[idx], point.Y() + direction_array_y[idx])
+
+            if self.validate_point(neighbour_cell_pos):
+                if(self.get_status_of_cell_at_position(neighbour_cell_pos) == Status.ALIVE):
+                    alive_neighbours += 1
+        
+        return alive_neighbours
+    
+    def validate_point(self, point):
+        if(point.X() >=0 and point.X() < self.rows):
+            if (point.Y() >=0 and point.Y() < self.columns):
+                return True
+            else:
+                return False
+        else:
+            return False
+        
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,10 +1,9 @@
+import unittest
+from game.status import Status
+from game.board import Board
+from game.point import Point
 import sys
 sys.path.append('../')
-from game.point import Point
-from game.board import Board
-from game.status import Status
-import unittest
-
 
 
 class TestBoardClass(unittest.TestCase):
@@ -22,3 +21,26 @@ class TestBoardClass(unittest.TestCase):
             )][sample_points[i].Y()].get_state()
             expected_status = Status.ALIVE
             self.assertEqual(expected_status, actual_status)
+
+    def test_validate_point(self):
+
+        sample_row = 2
+        sample_columns = 4
+        sample_alive_points = []
+
+        obj = Board(sample_row, sample_columns, sample_alive_points)
+
+        self.assertTrue(obj.validate_point(Point(1, 3)))
+        self.assertFalse(obj.validate_point(Point(1, 5)))
+
+    def test_get_count_of_alive_neighbours_of_cell(self):
+
+        sample_row = 2
+        sample_columns = 4
+        sample_alive_points = [Point(0, 0), Point(1, 1)]
+
+        obj = Board(sample_row, sample_columns, sample_alive_points)
+        test_point = Point(0, 0)
+        expected_alive_neighbours = 1
+        self.assertEqual(obj.get_count_of_alive_neighbours_of_cell(
+            test_point), expected_alive_neighbours)
